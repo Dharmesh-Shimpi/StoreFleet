db6eb3032914e86b112566ddd81527547b251a7c
Dharmesh Shimpi <161144920+Dharmesh-Shimpi@users.noreply.github.com>
2024-07-16 22:39:44 +0530

    initial

---
+++ src/product/model/product.repository.js
@@ -0,0 +1,38 @@
+import ProductModel from "./product.schema.js";
+
+export const addNewProductRepo = async (product) => {
+  return await new ProductModel(product).save();
+};
+
+export const getAllProductsRepo = async (page , pageSize) => {
+  
+  const skip = (page - 1) * pageSize;
+  
+  return await ProductModel.find({}).skip(skip)
+  .limit(pageSize)
+  .exec();
+};
+
+export const updateProductRepo = async (_id, updatedData) => {
+  return await ProductModel.findByIdAndUpdate(_id, updatedData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: true,
+  });
+};
+
+export const deleProductRepo = async (_id) => {
+  return await ProductModel.findByIdAndDelete(_id);
+};
+
+export const getProductDetailsRepo = async (_id) => {
+  return await ProductModel.findById(_id);
+};
+
+export const getTotalCountsOfProduct = async () => {
+  return await ProductModel.countDocuments();
+};
+
+export const findProductRepo = async (productId) => {
+  return await ProductModel.findById(productId);
+};
